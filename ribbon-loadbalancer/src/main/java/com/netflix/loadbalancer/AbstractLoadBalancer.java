6d25870399c8e31b4e74f824c4f16b8b43ad4360
limeng <limeng01@jianke.com>
2020-08-30 20:09:21 +0800

    AbstractLoadBalancer增加注释

--- ribbon-loadbalancer/src/main/java/com/netflix/loadbalancer/AbstractLoadBalancer.java
+++ ribbon-loadbalancer/src/main/java/com/netflix/loadbalancer/AbstractLoadBalancer.java
@@ -28,13 +28,20 @@ import java.util.List;
  * defines and implements a LoadBalacing Strategy via <code>IRule</code> 3. A
  * Class that defines and implements a mechanism to determine the
  * suitability/availability of the nodes/servers in the List.
- * 
- * 
+ *
+ * 一个LoadBalancer包含三个部分:
+ * 1.服务列表(根据具体的一些标准被分开)
+ * 2.通过IRule策略实现LoadBalacing
+ * 3.定义一种机制来确定类别中服务的可用性
+ *
  * @author stonse
  * 
  */
 public abstract class AbstractLoadBalancer implements ILoadBalancer {
-    
+
+    /**
+     * 定义三个服务组
+     */
     public enum ServerGroup{
         ALL,
         STATUS_UP,
@@ -43,6 +50,9 @@ public abstract class AbstractLoadBalancer implements ILoadBalancer {
         
     /**
      * delegate to {@link #chooseServer(Object)} with parameter null.
+     *
+     * 增加一个chooseServer()方法没有任何参数，然后调用父类的chooseServer()方法，参数为null
+     *
      */
     public Server chooseServer() {
     	return chooseServer(null);
@@ -51,13 +61,18 @@ public abstract class AbstractLoadBalancer implements ILoadBalancer {
     
     /**
      * List of servers that this Loadbalancer knows about
-     * 
+     *
+     * 抽象方法，根据服务组获取服务列表
+     *
      * @param serverGroup Servers grouped by status, e.g., {@link ServerGroup#STATUS_UP}
      */
     public abstract List<Server> getServerList(ServerGroup serverGroup);
     
     /**
      * Obtain LoadBalancer related Statistics
+     *
+     *抽象方法，获取与负载平衡器相关的统计信息
+     *
      */
     public abstract LoadBalancerStats getLoadBalancerStats();    
 }
